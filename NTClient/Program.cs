69462559389ab1eb4e99c3037efb084258103c6e
vainamov <vainamo@git.festival.ml>
2018-05-07 06:33:10 +0200

    Checkout

--- NTClient/Program.cs
+++ NTClient/Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NTClient
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args) {
+        private static void Main(string[] args) {
+            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect("localhost", 42420);
+
+            socket.Send(Encoding.UTF8.GetBytes(Enumerable.Repeat("spam", 1024).Aggregate("", (c, n) => c + " " + n) + "<EOF>"));
+
+            Console.ReadLine();
         }
     }
 }
